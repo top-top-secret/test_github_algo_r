32586f228910729d3c8df241d71e95c1ef040c9f
Ryan Miller <ryan@ryanandrewmiller.com>
2019-12-17 12:14:03 -0800

    Update test_github_algo_r.r

--- src/test_github_algo_r.r
+++ src/test_github_algo_r.r
@@ -3,5 +3,5 @@ library(algorithmia)
 # API calls will begin at the algorithm() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 algorithm <- function(input) {
-    paste("hello", input)
+    paste("hello from Github,", input)
 }
